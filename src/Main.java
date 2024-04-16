1219d15d7c076080431384d3c38e8905cce880c0
Uriel Campaña <urielcamp@gmail.com>
2024-04-16 14:41:07 -0300

    push primer ejercicio

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Ejercicio 1
+
+    Operaciones o1 = new Operaciones();
+
+    System.out.println(o1.sumar(2, 4));
+        System.out.println();
+        System.out.println(o1.sumar(2.3, 3.3));
+        System.out.println(o1.sumar(1, 2, 3, 4));
+
+        //Ejercicio 2
+
+    }
+}
---
+++ src/Operaciones.java
@@ -0,0 +1,26 @@
+public class Operaciones {
+
+
+    public Operaciones(){
+
+    }
+
+
+    //Metodos
+    public int sumar( int a, int b){
+        return a + b;
+
+
+
+    }
+
+    public int sumar(int a, int b, int c, int d){
+        return a + b + c + d;
+    }
+
+    public double sumar( double a, double b){
+        return a + b;
+
+    }
+
+}
